e982a91d9e201fdc9d20d0a17a4da0902c99911a
Olegnd <olegnd63@gmail.com>
2016-03-09 16:36:30 +0200

    task 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-﻿'use strict';
+'use strict';
 
-var car = require('js/car.js');
+//var car,put,land;
+import {car,put,land,drive} from 'js/car.js';
 
-console.log(car)
+    quantityPut = 0,
+    quantityLand = 0;
+    
+    put();
+    quantityPut = quantityPut + 1;
+    put();
+    quantityPut = quantityPut + 1;
+    land();
+    quantityLand = quantityLand + 1;
+    drive(90);
+console.log('quantityPut = '+quantityPut+' quantityLand = '+quantityLand);
+console.log('car in index '+car);
--- src/js/car.js
+++ src/js/car.js
@@ -1,1 +1,25 @@
-﻿'use strict';
+'use strict';
+var pessengers = [],
+    newSpeedCar;
+    
+var car = {color:'white',dours:4,speed:0,defaultSpeed:60,maxSpeed:100,seats:4,
+           passengers:(pessengers.length)};
+console.log('car '+car);
+    if (pessengers.length < 1 ) {car.speed = 0 & console.log('car empty')};
+    if (pessengers.length <= car.seats) 
+       {function put() {pessengers.push(true)}
+     }
+    else {console.log('car full')
+     }
+    if (pessengers.length > 1)          
+       {function land(){pessengers = pessengers.shift}
+     }
+    newSpeedCar = car.defaultSpeed;
+    if (newSpeedCar <= car.maxSpeed & pessengers.length > 1) 
+       {function drive(newSpeed) {newSpeedCar = newSpeed}
+        car.speed = newSpeedCar
+     }
+    else {car.speed = car.maxSpeed}
+
+
+export {car,put,land,drive};
