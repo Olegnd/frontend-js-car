e374be27e29c6928d28dc72744cafe4a3a41336c
Olegnd <olegnd63@gmail.com>
2016-03-10 18:01:54 +0200

    task 5

--- src/js/car.js
+++ src/js/car.js
@@ -7,35 +7,30 @@ var color         = 'color',
     maxSpeed      = 'maxSpeed',
     seats         = 'seats',
     pessengers    = [];
-var car = {color:'white',dours:4,speed:0,defaultSpeed:60,maxSpeed:100,
-    seats:4,pessengers:0}
 
-// quantityPessengers = 0;
-if (car.pessengers === 0) {
-        car.speed = 0;
+var car = {color:'white',dours:4,speed:0,defaultSpeed:60,maxSpeed:100,
+    seats:4,pessengers:pessengers.length}
+if (car.pessengers === 0) {car.speed = 0;
      }
-function put() {
-    if (car.pessengers < car.seats){
-        pessengers.push(true);
-        car.pessengers = pessengers.length;
-     }
+function put() {if (car.pessengers < car.seats){
+                    pessengers.push(true);
+                    car.pessengers = pessengers.length;
+                 }
 }
-function land(){
-    if (car.pessengers > 0) {
-        pessengers.shift();
-        car.pessengers = pessengers.length;
-     }
+function land(){if (car.pessengers > 0) {
+                    pessengers.shift();
+                    car.pessengers = pessengers.length;
+                 }
 }
-function drive(newSpeed) {
-    if (newSpeed <= car.maxSpeed
-        & car.pessengers > 0){
-        car.speed = newSpeed;
-     }
-    else { if (car.pessengers > 0) {
-              car.speed = car.maxSpeed
-            }
-           else {car.speed = 0}
-     }
+function drive(newSpeed) {if (newSpeed <= car.maxSpeed
+                              && car.pessengers > 0){
+                              car.speed = newSpeed;
+                            }
+                          else {if (car.pessengers > 0) {
+                                 car.speed = car.maxSpeed
+                                 }
+                                else {car.speed = 0}
+                            }
 }
 export {car,put,land,drive,color,
         dours,speed,defaultSpeed,maxSpeed,seats,pessengers};
