4fa97f763e1e2f5f84a191bef31b825d089528de
Olegnd <olegnd63@gmail.com>
2016-03-11 09:19:45 +0200

    modification 7

--- src/js/car.js
+++ src/js/car.js
@@ -2,24 +2,22 @@
 // code Olegnd
 var pessengers = [];
 var car        = {color:'white',dours:4,speed:0,defaultSpeed:60,
-                  maxSpeed:100,seats:4,pessengers:pessengers.length}
+                  maxSpeed:100,seats:4,pessengers:pessengers}
 if (car.pessengers === 0) {car.speed = 0;
  }
-function put() {if (car.pessengers < car.seats){
+function put() {if (pessengers.length < car.seats){
                     pessengers.push(true);
-                    car.pessengers = pessengers.length;
                  }
 }
-function land() {if (car.pessengers > 0) {
+function land() {if (pessengers.length > 0) {
                     pessengers.shift();
-                    car.pessengers = pessengers.length;
                  }
 }
 function drive(newSpeed) {if (newSpeed <= car.maxSpeed
-                              && car.pessengers > 0)
+                              && pessengers.length > 0)
                            {car.speed = newSpeed;
                            }
-                          else {if (car.pessengers > 0) {
+                          else {if (pessengers.length > 0) {
                                  car.speed = car.maxSpeed
                                }
                                 else {car.speed = 0}
--- src/js/test_car.js
+++ src/js/test_car.js
@@ -7,31 +7,32 @@ var    quantityPut     =  0,
        speedometer     = [0,30,90,100,120],
        indexSpeedometr = 0;
 
-console.log('car = ',car);
+
 console.log('car = {'+'color:'+car.color+',dours:'+car.dours+',speed:'+car.speed+
             ',defaultSpeed:'+car.defaultSpeed+',maxSpeed:'+car.maxSpeed+
-            ',seats:'+car.seats+',pessengers[]:'+car.pessengers+'}');
+            ',seats:'+car.seats+',pessengers:'+car.pessengers+'   !!! pessenger []}');
            
 console.log('__________________________________________________________________________________________');
 
 // put pessenger
+       console.log('pessengers  = ',pessengers);
 while (quantityPut < 6){
        put();
-       console.log('pessengers  = ',pessengers,' car.pessengers = '+car.pessengers);
        quantityPut = quantityPut + 1;
+       console.log('pessengers  = ',pessengers,' quantityPut = ',quantityPut);
 }
 
 // land pessenger
 while (quantityLand < 5){
        land();
-       console.log('pessengers  = ',pessengers,' car.pessengers = '+car.pessengers);
        quantityLand = quantityLand + 1;
+       console.log('pessengers  = ',pessengers,' quantityLand = ',quantityLand);
 }
 
 // change speed
     put();
     quantityPut = quantityPut + 1;
-    console.log('pessengers  = ',pessengers,' car.pessengers = '+car.pessengers);
+    console.log('pessengers  = ',pessengers,' quantityPut = ',quantityPut);
 while (indexSpeedometr < speedometer.length){
        drive(speedometer[indexSpeedometr]);
        console.log('speedometr = ',speedometer[indexSpeedometr]+' car.speed = '+car.speed);
@@ -41,7 +42,7 @@ while (indexSpeedometr < speedometer.length){
        quantityLand = quantityLand + 1;
        indexSpeedometr = indexSpeedometr - 1;
        drive(speedometer[indexSpeedometr]);
-       console.log('pessengers = ',pessengers,' car.pessengers = '+car.pessengers);
+       console.log('pessengers  = ',pessengers,' quantityLand = ',quantityLand);
        console.log('speedometr = ',speedometer[indexSpeedometr]+' car.speed = '+car.speed);
 console.log('__________________________________________________________________________________________');
 console.log('quantityPut = '+quantityPut+' quantityLand = '+quantityLand+' declared by speed = '+speedometer[4]);
