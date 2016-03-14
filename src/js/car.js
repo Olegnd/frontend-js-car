457b571ccbbae8e2334aab20ef2fd92303068504
Olegnd <olegnd63@gmail.com>
2016-03-14 16:11:32 +0200

    modification 18

--- src/js/car.js
+++ src/js/car.js
@@ -7,7 +7,7 @@ var car        = {color:'white',dours:4,speed:0,defaultSpeed:60,
                   maxSpeed:100,seats:4,pessengers:pessengers,
 				 put:undefined,land:undefined,drive:undefined};
 
-if (pessengers.length === 0) {car.speed = 0}
+
  
 car.put = function put() {if (pessengers.length < car.seats){
                     pessengers.push(true);
