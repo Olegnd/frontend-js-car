39c90d598edd01dadc1bbd5254662d829ae1c755
Olegnd <olegnd63@gmail.com>
2016-03-16 20:49:34 +0200

    modification 21

--- src/index.js
+++ src/index.js
@@ -3,16 +3,5 @@
 
 var car = require('js/car.js');
 
-//var test = require('js/test_car.js');
-//test.testPutPessengers(car.put);
-//test.testLandPessengers(car.land);
-//test.testChangeSpeed(car.drive);
 
-//car.put();
-//car.put();
-//car.land();
-//car.drive(90);
-
-
-//console.log('car = ',car);
 
--- src/js/car.js
+++ src/js/car.js
@@ -1,22 +1,52 @@
 'use strict';
 // code Olegnd
+
 var car = {color:'white',dours:4,speed:0,defaultSpeed:60,
            maxSpeed:100,seats:4};
-car.pessengers = [];
-car.put = function put() {if (this.pessengers.length < car.seats)
-                             {this.pessengers.push(true)}
-console.log('this.pessenger = ' + this.pessengers);}
+car.passengers = [];
 
-car.land = function land() {if (this.pessengers.length > 0)
-                               {this.pessengers.shift()}
-console.log('this.pessenger = ' + this.pessengers);}
 
-car.drive = function drive(newSpeed) {if (this.pessengers.length <= 0) {this.speed = 0}
-						              else {if (newSpeed  <= this.maxSpeed)
-								               {this.speed  = newSpeed}
-                                            else {this.speed  = this.maxSpeed}}
-console.log('this.speed = ' + this.speed);}
+car.put = function put() {if (this.passengers.length <= 0)
+                             {this.passengers.push(true); 
+                              this.speed = this.defaultSpeed}
+                          else {if (this.passengers.length < this.seats) 
+                                   {this.passengers.push(true)}
+                                else{}
+                               }
+//console.log('this.pessenger = ' + this.passengers);
+//console.log('this.speed = ' + this.speed);
+//console.log('____________________________________________');
+                         }
+
+
+
+car.land = function land() {if (this.passengers.length >= 2)
+                               {this.passengers.shift()}
+                            else {this.passengers.shift(); car.speed = 0;}
+                            
+//console.log('this.pessenger = ' + this.passengers);
+//console.log('this.speed = ' + this.speed);
+//console.log('____________________________________________');
+                           }
+
+car.drive = function drive(newSpeed) 
+                          {if (this.passengers.length === 0) {this.speed = 0}
+                           else {if (newSpeed  <= this.maxSpeed)
+                                    {this.speed  = newSpeed}
+                                 else {this.speed  = this.maxSpeed}}
+                           if (newSpeed === undefined && this.passengers.length ===0) 
+                               {this.speed = 0}
+                           else {if (newSpeed === undefined) {this.speed = this.defaultSpeed}
+                                }
+//console.log('newSpeed       = ' + newSpeed);
+//console.log('this.pessenger = ' + this.passengers);
+//console.log('this.speed     = ' + this.speed);
+//console.log('____________________________________________');
+                          }
+
 module.exports = car;
+
+//&& 
 
 
 
